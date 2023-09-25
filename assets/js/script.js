c03da349e3731bd63b226ecc4982a6327a5ab5f7
Conal2023 <>
2023-09-25 13:08:26 +0000

    Add template forplayer vs computer choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 function play(playerChoice) {
     const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    let resultMessage = '';
+
+    if (playerChoice === computerChoice) {
+        resultMessage = `It's a tie!. Computer chose ${computerChoice} too! Go again!!`
+    }
+    else if (
+        (playerChoice === '' && (computerChoice === '' || computerChoice  === '')) ||
+        (playerChoice === '' && (computerChoice === '' || computerChoice === '')) ||
+        (playerChoice === '' && (computerChoice === '' || computerChoice === '')) ||
+        (playerChoice === '' && (computerChoice === '' || computerChoice === '')) ||
+        (playerChoice === '' && (computerChoice === '' || computerChoice === ''))
+    )
 }
